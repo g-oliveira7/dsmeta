76f6112980bebd1718f99eeed4d81e662691840f
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2022-09-05 18:32:40 -0300

    Project created

---
+++ backend/src/test/java/br/com/studying/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studying.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

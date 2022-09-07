d507eeb5fa232c46705bf66bf27a87d43aea066e
g-oliveira7 <studying.online@outlook.com>
2022-09-07 15:50:32 -0300

    System properties file

--- backend/src/main/java/br/com/studying/dsmeta/services/SmsService.java
+++ backend/src/main/java/br/com/studying/dsmeta/services/SmsService.java
@@ -1,7 +1,5 @@
 package br.com.studying.dsmeta.services;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
